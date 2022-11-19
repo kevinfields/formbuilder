0c12ebd8376228b90aa7cc39c62d133054cf6b74
Kevin Fields <kfields31@gmail.com>
2022-11-19 11:39:22 -0600

    basic form builder screen structure

---
+++ src/components/FormBuilderScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styling/FormBuilder.css";
+
+const FormBuilderScreen = (props) => {
+  return (
+    <div className='form-builder-screen'>
+      <h2 className='form-builder-header'>Build a Form</h2>
+    </div>
+  )
+}
+
+export default FormBuilderScreen
